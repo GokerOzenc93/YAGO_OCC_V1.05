c8f044553769170cc45679f630972281298c9014
GokerOzenc93 <gokerozenc@gmail.com>
2026-04-10 23:15:05 +0300

    Updated Scene.tsx

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -515,7 +515,7 @@ const Scene: React.FC = () => {
             margin right=116, margin bottom=140
             Extra bottom margin gives room for the now-larger compass ring + labels
           */}
-          <GizmoHelper alignment="bottom-right" margin={[145, 120]}>
+          <GizmoHelper alignment="bottom-right" margin={[145, 100]}>
             <group scale={42}>
               <ViewCube />
             </group>
