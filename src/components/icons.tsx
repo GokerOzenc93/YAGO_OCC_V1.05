f5df0e051ac7083cb9e07beaf4841c69fbc84bff
GokerOzenc93 <gokerozenc@gmail.com>
2026-05-02 20:40:10 +0300

    Updated icons.tsx

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -16,15 +16,13 @@ export const AddBoxButton: React.FC<IconButtonProps> = ({ onClick, disabled = fa
     title="Kutu Ekle (B)"
   >
     <svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M0.730769 0.365385C0.730769 0.163589 0.894358 0 1.09615 0H18.6346C18.8364 0 19 0.163588 19 0.365385V17.9038C19 18.1056 18.8364 18.2692 18.6346 18.2692H1.09615C0.894358 18.2692 0.730769 18.1056 0.730769 17.9038V0.365385Z" fill="#ffffff"/>
-      <path d="M2.19231 6.21154V16.8077H12.4231V6.21154H2.19231Z" stroke="black" strokeWidth="0.216"/>
-      <path d="M2.19231 6.21154L7.125 2.00962M12.4231 6.21154L16.9904 2.00962M12.4231 16.8077L16.9904 12.4231" stroke="black" strokeWidth="0.216"/>
-      <path d="M7.125 2.00962H16.9904V12.4231" stroke="black" strokeWidth="0.216"/>
+      <path d="M1 0.360001C1 0.161178 1.16118 0 1.36 0H18.64C18.8388 0 19 0.161178 19 0.36V17.64C19 17.8388 18.8388 18 18.64 18H1.36C1.16118 18 1 17.8388 1 17.64V0.360001Z" fill="#F0EBEB"/>
+      <path d="M2.44 6.12V16.56H12.52V6.12H2.44Z" stroke="black" strokeWidth="0.7"/>
+      <path d="M2.44 6.12L7.3 1.98M12.52 6.12L17.02 1.98M12.52 16.56L17.02 12.24" stroke="black" strokeWidth="0.7"/>
+      <path d="M7.3 1.98H17.02V12.24" stroke="black" strokeWidth="0.7"/>
       <g style={{ mixBlendMode: 'plus-darker' }}>
-        <path d="M6.86619 12.1338H3.12099V10.8854H6.86619V7.14022H8.11458V10.8854H11.8598V12.1338H8.11458V15.879H6.86619V12.1338Z" fill="#ffffff"/>
+        <path d="M6.765 11.995H3.075V10.765H6.765V7.075H7.995V10.765H11.685V11.995H7.995V15.685H6.765V11.995Z" fill="#FF8D28"/>
       </g>
     </svg>
   </button>
 );
-
-https://www.figma.com/design/3TsDPlX1aJRahMhjmHrB0K/Add-Box?node-id=3-85&t=dBM6zxEgjTyFkC6U-1
