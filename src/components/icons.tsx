d68f9ea81019b1b79986b93fd619b78b51816acf
GokerOzenc93 <gokerozenc@gmail.com>
2026-05-02 20:43:42 +0300

    Updated icons.tsx

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -17,7 +17,7 @@ export const AddBoxButton: React.FC<IconButtonProps> = ({ onClick, disabled = fa
   >
     <svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path d="M1 0.360001C1 0.161178 1.16118 0 1.36 0H18.64C18.8388 0 19 0.161178 19 0.36V17.64C19 17.8388 18.8388 18 18.64 18H1.36C1.16118 18 1 17.8388 1 17.64V0.360001Z" fill="#ffffff"/>
-      <path d="M2.44 6.12V16.56H12.52V6.12H2.44Z" stroke="black" strokeWidth="0.7"/>
+      <path d="M2.44 6.12V16.56H12.52V6.12H2.44Z" stroke="black" strokeWidth="1.0"/>
       <path d="M2.44 6.12L7.3 1.98M12.52 6.12L17.02 1.98M12.52 16.56L17.02 12.24" stroke="black" strokeWidth="1.0"/>
       <path d="M7.3 1.98H17.02V12.24" stroke="black" strokeWidth="1.0"/>
       <g style={{ mixBlendMode: 'plus-darker' }}>
